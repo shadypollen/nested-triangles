4a5eab9a993010afc0ab6d257d009afc2a8533a0
shadypollen <coplayr@hotmail.com>
2017-11-19 05:37:10 +0000

    An ugly and yet working version of the nested triangles generator

--- nestedTriangles.py
+++ nestedTriangles.py
@@ -17,6 +17,7 @@ def main():
 	triangleGenerator(triangleWidth, nestLevel)
 
 
+#Generates basic un-nested triangles and puts them in a list
 def triangleGenerator(triangleWidth, nestLevel):
 	charOne = "*"#charString[0]
 	charTwo = "."#charString[1]
@@ -24,6 +25,7 @@ def triangleGenerator(triangleWidth, nestLevel):
 	triangleRow = []
 
 	for level in range(nestLevel):
+		#alternates nest chars every level
 		if level % 2 == 0:
 			char = charOne
 		else:
@@ -36,7 +38,8 @@ def triangleGenerator(triangleWidth, nestLevel):
 		if level % 2 == 0:	
 			triangleList.append(triangleRow)
 		else:
-			triangleList.append(list(reversed(triangleRow)))
+#reverses triangle direction every level
+			triangleList.append(list(reversed(triangleRow))) 
 		
 		triangleRow = []
 		triangleWidth = (triangleWidth*2)+1
@@ -45,7 +48,8 @@ def triangleGenerator(triangleWidth, nestLevel):
 		joinTriangle(triangleList, nestLevel)
 
 
-def printTriangle(triangleList):
+#prints basic un-nested triangles for reference
+def printTriangle(triangleList): 
 	for triangle in triangleList:
 		print("Level:", triangleList.index(triangle)+1)
 		for row in triangle:
@@ -54,28 +58,55 @@ def printTriangle(triangleList):
 def joinTriangle(triangleList, nestLevel):
 	triOneIndex = 0
 	triTwoIndex = 1
-	resultTriangle = []
-	triangleOne = triangleList[triOneIndex]
-	triangleTwo = triangleList[triTwoIndex]	
-	counter = 0
-	for row in reversed(triangleOne):
-		interTri = []
-		nestRow = list(reversed(triangleTwo))[counter]
-		nestInterRow = nestRow.replace(" ", "")
-		nestingRow = row.replace(" ", "")
-		nestChar = nestInterRow[0][0]
-		interTri.append(nestingRow.center(len(nestInterRow), nestChar))
-		resultTriangle.append(interTri[0].center(len(nestRow), " "))
-		interTri = []			
-		counter = counter + 1
-	nestEnd = len(resultTriangle)
-	for row in reversed(triangleTwo[:nestEnd]):
-		resultTriangle.append(row)
-	print("Result triangle:")
-	for row in reversed(resultTriangle):
-		print(row)
-#	triOneIndex = triOneIndex + 1
-#	triTwoIndex = triTwoIndex + 1
+	resultIndex = 1
+
+	for level in range(nestLevel-1):
+		resultTriangle = []
+		triangleOne = triangleList[triOneIndex]
+		triangleTwo = triangleList[triTwoIndex]	
+		counter = 0
+		print("level:", level, "resultIndex", resultIndex)
+		if level % 2 == 0:
+			for row in reversed(triangleOne):
+				interTri = []
+				nestRow = list(reversed(triangleTwo))[counter]
+				nestInterRow = nestRow.replace(" ", "")
+				nestingRow = row.replace(" ", "")
+				nestChar = nestInterRow[0][0]
+				interTri.append(nestingRow.center(len(nestInterRow), nestChar))
+				resultTriangle.append(interTri[0].center(len(nestRow), " "))
+				interTri = []			
+				counter = counter + 1
+			nestEnd = len(resultTriangle)
+			for row in reversed(triangleTwo[:nestEnd]):
+				resultTriangle.append(row)
+			print("Result triangle:")
+			for row in reversed(resultTriangle):
+				print(row)
+			triangleList[resultIndex] = reversed(resultTriangle)
+		else:
+			for row in triangleOne:
+				interTri = []
+				nestRow = list(triangleTwo)[counter]
+				nestInterRow = nestRow.replace(" ", "")
+				nestingRow = row.replace(" ", "")
+				nestChar = nestInterRow[0][0]
+				interTri.append(nestingRow.center(len(nestInterRow), nestChar))
+				resultTriangle.append(interTri[0].center(len(nestRow), " "))
+				interTri = []
+				counter = counter + 1
+			nestEnd = len(resultTriangle)
+			for row in triangleTwo[nestEnd:]:
+				resultTriangle.append(row)
+			print("Result triangle odd:")
+			for row in resultTriangle:
+				print(row)
+			triangleList[resultIndex] = resultTriangle
+		triangleList[level] = list(reversed(resultTriangle))
+		triOneIndex = triOneIndex + 1
+		triTwoIndex = triTwoIndex + 1
+		resultIndex = resultIndex + 1
+		
 	
 	
 main()
